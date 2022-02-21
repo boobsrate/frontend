75fb73f6c6d24db33aa0b436f7dc0a09c7dd332b
s1veme <kpdpav@gmail.com>
2022-02-21 08:26:35 +0300

    Moved the button "murrengan" to the right

--- src/components/includes/Header.vue
+++ src/components/includes/Header.vue
@@ -2,16 +2,21 @@
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Boobs rate</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link" href="#">Main</a>
-          <a class="nav-link" href="#">Top 25</a>
-          <a class="nav-link" href="#">Top 100</a>
-          <a class="nav-link" href="#">Abyss</a>
-          <a class="nav-link" href="https://murrengan.ru"> Murrengan </a>
+          <div class="left">
+            <a class="nav-link" href="#">Main</a>
+            <a class="nav-link" href="#">Top 25</a>
+            <a class="nav-link" href="#">Top 100</a>
+            <a class="nav-link" href="#">Abyss</a>
+          </div>
+          <div class="right">
+            <a class="nav-link" href="https://murrengan.ru"> Murrengan </a>
+          </div>
         </div>
       </div>
     </div>
@@ -26,8 +31,17 @@ export default {
 
 <style scoped>
 
-.nav-link {
-
+.navbar-nav {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
 }
 
+.left {
+  display: flex;
+}
+
+.right {
+  display: flex;
+}
 </style>
